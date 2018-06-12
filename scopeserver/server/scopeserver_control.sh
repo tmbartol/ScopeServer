0f1608349657dd535b84db4388215c87fc2b9b7a
Tom Bartol <bartol@salk.edu>
2018-06-12 00:18:26 -0700

    Adjust button colors

--- scopeserver/server/scopeserver_control.sh
+++ scopeserver/server/scopeserver_control.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-/home/pi/src/scopeserver-git/scopeserver/server/scope_server_real.py &
+#/home/pi/src/scopeserver-git/scopeserver/server/scope_server_real.py &
+/home/pi/src/scopeserver-git/scopeserver/server/scope_server_sim.py &
 
